6ddb1b29ad7cea496d083d85c787ef565cc353de
John King <john@mettaaudio.com>
2012-06-19 12:05:47 -0400

    Updated for Spree 1.1.0

--- app/overrides/paypal_form.rb
+++ app/overrides/paypal_form.rb
@@ -1,4 +1,5 @@
-Deface::Override.new(:virtual_path => "spree/checkout/edit",
+Deface::Override.new(:virtual_path      => "spree/checkout/edit",
                      :surround_contents => "[data-hook='checkout_form_wrapper']",
-                     :text => "<% if pay_with_paypal? -%><%= render :partial => 'spree/checkout/paypal_checkout' %><% else -%><%= render_original %><% end -%>",
-                     :name => "paypal_form")
+                     :text              => "<% if pay_with_paypal? -%><%= render :partial => 'spree/checkout/paypal_checkout' %><% else -%><%= render_original %><% end -%>",
+                     :sequence          => {:after => "replace_checkout_edit"},
+                     :name              => "paypal_form")
